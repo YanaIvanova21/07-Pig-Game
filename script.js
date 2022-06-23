c996dabd113fc2d03bb72c9fc47354fbd3a89986
YanaIvanova21 <yana.ivanova.199621@gmail.com>
2022-06-23 15:19:34 +0300

    Switching active player fucntionality

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 'use strict';
 //Selecting elemets
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1'); //special selector for IDs, not obligatory to use
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -13,8 +16,9 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
-
+const score = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -29,8 +33,14 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to the current score
     currentScore += dice;
-    current0El.textContent = currentScore; //CHANGE LATER
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Select a new player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0; //if active player is 0, than it should change on active player 1, and vice versa
+    player0El.classList.toggle('player--active'); //toggle removes the class if it presents, and adds if it absent
+    player1El.classList.toggle('player--active'); //putting toggle for both elements interchange their states from active to non-active and vice versa
   }
 });
